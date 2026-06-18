b1401ec699c2f2653d36e479a6ee36458c5c3bc0
digitalseo135-png <digitalseo135@gmail.com>
2026-06-18 22:40:38 +0700

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,553 @@
+<!DOCTYPE html>
+<html lang="id-ID">
+<head>
+<title>Live Draw SGP Tercepat | Hasil Live Draw Singapore Hari Ini Lengkap</title>
+<meta name="description" content="Cek Live Draw SGP hari ini tercepat dan terlengkap. Update real-time hasil SGP resmi, data keluaran lengkap, panduan membaca result, serta FAQ seputar Live Draw SGP."/>
+<meta name="geo.placename" content="Indonesia" />
+<meta name="geo.region" content="ID" />
+<meta name="geo.country" content="ID" />
+<meta name="language" content="id-ID" />
+<meta name="tgn.nation" content="Indonesia" />
+<meta property="og:type" content="object" />
+<meta property="og:title" content="Live Draw SGP Tercepat | Hasil Live Draw Singapore Hari Ini Lengkap" />
+<meta property="og:description" content="Cek Live Draw SGP hari ini tercepat dan terlengkap. Update real-time hasil SGP resmi, data keluaran lengkap, panduan membaca result, serta FAQ seputar Live Draw SGP."/>
+<meta property="og:url" content="https://livedrawsgptoto.shop/" />
+<meta property="og:site_name" content="Live Draw SGP Tercepat | Hasil Live Draw Singapore Hari Ini Lengkap" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="google-site-verification" content="G2Li71HFucwngagY8Bb7Oj0-xrTzQgny7KKCBfv_wII" />
+<meta name="google-site-verification" content="mPEKZTfhbasOt6TT73cmtawU7TdYUUStFtj3eMzu6ts" />
+<meta name="google-site-verification" content="RbMO0AsX9LFvJnK3uHcDPzBB5p3Wu4poDf5O2UdJFaA" />
+<meta name="google-site-verification" content="wbhGoePb0MDUQLb5-Slzz0HHyWG-KSNtxiLr3OzoCHg" />
+<meta name="google-site-verification" content="u8EVIj81tX5FJSGddLdRV-AiuZybDzqbaaMgVO82qRk" />
+<meta name="google-site-verification" content="72pRpxe8iZICT7G9Z5STtUK6C9_xx9VDbu1W497LZ9E" />
+<meta name="google-site-verification" content="4Sy4Lf1AJOHzGF4kB_vAkd6HHEoHl310PyNmioNa33w" />
+<meta name="google-site-verification" content="aPl17hYAcCFNFktn5ktl3IUR60qGyLWpfpHvzIq3OwY" />
+<meta name="google-site-verification" content="7saZbLlajME2A6RKz81xlanXD77d14ldRkAqO1DbUUw" />
+<link rel="canonical" href="https://livedrawsgptoto.shop/">
+<link rel="icon" href="img/logofavsgp.png" sizes="32x32" />
+<link rel="icon" href="img/logofavsgp.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="img/logofavsgp.png" />
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic|Oswald:400,700" media="screen">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <style>div {
+	box-sizing: border-box;
+	
+}
+.btn-email {
+  display: inline-flex;
+  align-items: center;
+  gap: 8px;
+  background-color: #ffffff;
+  color: #1100ff;
+  padding: 5px 10px;
+  border-radius: 10px;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.icon-anim {
+  display: inline-block;
+  transition: 0.3s;
+}
+
+.btn-email:hover .icon-anim {
+  transform: scale(1.2) rotate(10deg);
+}
+body {
+	
+	background: black;
+	font-family: 'Oswald', sans-serif;
+}
+
+.space {
+	width: 100%;
+	height: 150px;
+}
+
+.container2 {
+	width: 65%;
+	margin: auto;
+		
+}
+
+.banner-float {
+	position: fixed;
+	width: 100%;
+	text-align: center;
+	bottom: 0px;
+	z-index: 9999;
+}
+
+.banner-float img {
+	width: 728px;
+	
+}
+
+.banner1 img {
+	display: inline-block;
+	width: 49%;
+}
+
+.floating-top {
+	background: transfarant; top: 0; height: auto; left: 0; padding: 0px; position: fixed; text-align: center; width: 100%; z-index: 1001;
+}
+.floating-bottom {
+	background: transfarant; bottom: 0; height: auto; left: 0; padding: 0px; position: fixed; text-align: center; width: 100%; z-index: 1001;
+}
+
+.floating-top img {
+	width: 49%;
+}
+.floating-bottom img {
+	width: 49%;
+}
+.headerbanner{
+text-align:center;
+}
+.headerbanner img {
+	width:49%;
+}
+
+.banner-top img {
+	width: 728px;
+	
+}
+
+.head {
+	width: 100%;
+	padding: 20px;
+	background: #000000;
+	border: 1px gray solid;
+	
+}
+
+.logo {
+  display: block;
+  width: 550px;
+  height: 200px;
+  background: url("/img/livedrawsgp.png") center no-repeat;
+  background-size: 100% 100%;
+  margin: auto;
+}
+
+.menu {
+	display: block;
+	width: 100%;
+	text-align: center;
+	background: linear-gradient(to bottom, #1100FF 0%,#1400ff 50%,#0040ff 100%);
+}
+    
+.menu a {
+	display: inline-block;
+	padding: 10px 20px;
+	text-decoration: none;
+	color: white;
+	margin: 0px;
+	font-size: 18px;
+}
+
+.menu a:hover {
+	color: white;
+}
+
+.content {
+	width: 100%;
+	border: 1px white solid;
+}
+
+h1, h2, h3 {
+	text-align: center;
+	
+}
+
+h3 {
+	font-family: arial;
+	margin: 40px 0px;
+	color: white;
+	font-size: 28px;
+}
+
+.title-head {
+	font-size: 24px;
+	text-decoration: none;
+	color: white;
+	
+}
+
+.title-footer {
+	font-size: 28px;
+	text-decoration: none;
+	color: white;
+}
+
+.content p {
+	text-align: left;
+	font-size: 16px;
+	color: white;
+	margin: 10px;
+}
+
+.iklan {
+	width: 100%;
+	
+}
+
+.iklan img {
+	width: 100%;
+	
+	
+}
+
+
+.t1 {
+	width: 40%;
+	padding: 5px 0px 5px 10px;
+	border: 1px solid white;
+	background: white;
+	font-size: 16px;
+}
+
+.t2 {
+	width: 30%;
+	padding: 5px 0px 5px 10px;
+	border: 1px solid white;
+	background: white;
+	font-size: 16px;
+}
+
+.t3 {
+	width: 30%;
+	padding: 5px 0px 5px 10px;
+	border: 1px solid white;
+	background: white;
+	font-size: 16px;
+}
+
+.lassres {
+	width: 65%;
+	margin: auto;
+	box-sizing: border-box;
+}
+
+.lassres table {
+	width: 100%;
+	border: 1px solid white;	
+}
+
+.lassres img{
+	max-width: 100%;
+	height: auto;
+	padding: 1px 0 0 0;
+	
+}
+
+.out-link {
+	list-style-type: square;
+	margin:  0px 35px;
+	color: #FF751F;
+}
+
+.out-link a {
+	text-decoration: none;
+	color: #FF751F;
+}
+
+a {
+    color:white;
+    text-decoration: none;
+}
+
+a:hover {
+    color: #1100FF;
+}
+
+
+
+@media only screen and (max-device-width: 1000px) {
+	
+.floating-top img {
+width:100%
+}
+.floating-bottom img {
+width:100%
+}
+  .headerbanner img{
+  width:100%;
+  }
+
+	.container2 {
+		width: 100%;
+		
+		
+	}
+	
+	.logo {
+	width: 280px;	
+	height: 80px;
+	margin: auto;
+	}
+	
+	.menu a {
+		display: block;
+		font-size: 16px;
+		border: 1px solid white;
+	}
+	
+	.banner1 {
+		display: block;
+		
+	}
+	
+	.banner1 img {
+		display: block;
+		width: 100%;
+	}
+	
+	.lassres {
+	width: 100%;
+	margin: auto;
+	
+    }
+
+    .lassres tableresult {
+	width: 100%;
+	padding: 0 0 0 0;
+	border: 1px solid white;	
+    }
+
+    .lassres img {
+    width: 100%;
+    height: auto;
+    padding: 1px 0 0 0;
+    }
+	
+	
+	.banner2 {
+		display: block;
+		
+	}
+	
+	.banner2 img {
+		display: block;
+		width: 100%;
+	}
+	
+	.banner {
+	width: 100%;
+	box-sizing: border-box;
+	text-align: center;
+	background: #3a3a3a;
+}
+
+.banner img {
+	display: inline-block;
+	width: 48%;
+	box-sizing: border-box;
+	margin: 5px;
+}
+
+.banner-float {
+	position: fixed;
+	width: 100%;
+	text-align: center;
+	bottom: 1px;
+	z-index: 9999;
+}
+
+.banner-float img {
+	width: 728px;
+	
+}
+
+	h3 {
+		font-size: 14px;
+		
+	}
+	
+	.title-footer {
+		font-size: 14px;
+		
+	}
+	
+	.banner-float {
+		width: 100%;
+		
+	}
+	
+  .banner-top {
+		width: 100%;
+		
+	}
+
+	.banner-float img {
+		width: 100%;
+		
+	}
+	
+	.banner-top img {
+		width: 100%;
+		
+	}
+
+}
+
+@media only screen and (max-device-width: 550px) {
+	.logo {
+	width: 205px;
+    margin: auto;        
+	}  
+	
+.banner1 img {
+		display: inline-block;
+		width: 100%;
+	}
+}  
+.banner1 {
+		display: block;
+		text-align:center;
+	}
+	
+.banner img {
+		display: block;
+		width: 100%;
+		margin: 0px;
+	}
+	.banner-float {
+		width: 100%;
+		
+	}
+	
+	.banner-float img {
+		width: 100%;
+		
+	} 
+
+.asu{font-weight:100}.e1,.e2,.e3,.e4,.e5,.e6,.e7,.e8,.e9,.e0{background-color:#ff4d4d;border-radius:0;color:#fff}.k1,.k2,.k3,.k4,.k5,.k6,.k7,.k8,.k9,.k0{background-color:green;border-radius:0;color:#fff}.c1,.c2,.c3,.c4,.c5,.c6,.c7,.c8,.c9,.c0{background-color:blue;border-radius:0;color:#fff}.a1,.a2,.a3,.a4,.a5,.a6,.a7,.a8,.a9,.a0{background-color:orange;border-radius:0;color:#fff}.highlight{padding:1px 4px;margin:0 -4px}input.cari{width:50px;text-align:center;color:red;font-weight:700;background:#f0f7f2}#rb{background:#f03a17}
+
+fieldset {border: none;margin: 0;padding: 0;list-style: none;vertical-align: baseline;}  .head th{background:#9C1512;color:#fff}.entry-content th,.entry-content td,.comment-content th,.comment-content td{padding:0!important}textarea{width:100%}.asux{background:none repeat scroll 0 0 #fff;color:#d7d1d1;font-weight:400}tr:nth-last-child(5n+1){background:none repeat scroll 0 0 #e8e4e7!important}.colormenu{background:none 0 0 repeat scroll #d9d9d9;border:2px solid #d9d9d9;width:796px;height: 30px; center: unset;z-index:99999;padding:0;max-width:100%;margin-bottom:10px}.colormenu.fixed{position:fixed;height: 30px; center: unset;width: 796px;top:0;left:0}.intro td{background:none repeat scroll 0 0 #ffffff!important}table{background:#fff none repeat scroll 0 0;margin:1px;min-width:500px}#drawing-table thead td{background: #dd9933!important;width:100%;background: #0000f4 !important;margin:0;margin-top:10px}#drawing-table td {border: 1px solid #d9d9d9;text-align: center;padding: 0;font-family: 'Roboto', sans-serif;font-size: 13px;font-weight: bold;padding: 3px 0 3px 0;}#color-selector .color{float:left;height:25px;position:relative;width:11%;cursor:pointer}#color-selector .color.eraser{background:none repeat scroll 0 0 #fff}#color-selector .color.Blue{background:#43a2ff}#color-selector .color.Aqua{background:#00F5FF}#color-selector .color.Teal{background:#43CD80}#color-selector .color.Lime{background:#01FF70}#color-selector .color.Yellow{background:#FFDC00}#color-selector .color.Orange{background:#FF851B}#color-selector .color.Red{background:#FF4136}#color-selector .color.Pink{background:#F012BE}#color-selector .color.Purple{background:#A23BEC}#color-selector .color.Silver{background:#a9a9a9}#color-selector .selected{box-shadow:inset 0 0 0 2px #000}#btnSubmit{float:left;margin-top:0;font-size:9px;background:#333;color:#FFF;font-weight:600;border:none;height:25px;line-height:25px;width:10%;cursor:pointer;text-transform:uppercase}#table-wrap{font-weight:700;position:relative}#table-wrap table{position:relative;z-index:10}#toggle-tracing-mode{display:none}#html-wrap textarea{height:50px;margin:0 0 10px;overflow:auto;width:100%}#btnSubmit{float:left;margin-top:0}@media (max-width:767px){#drawing-table{min-width:100%;margin-right:-10px;margin-left:-10px}}@media (max-width:370px){#drawing-table td{font-size:10px}}
+tr.headd {
+    background: linear-gradient(135deg, #000000 0%, #000000 50%, #000000 51%, #000000 100%);
+    color: white;
+
+
+</style>
+</head>
+
+<body>
+
+<div class="container2">
+<div class="head">
+
+<a class="logo" href="https://livedrawsgptoto.shop/" title="Live Draw SGP"></a>
+
+</div>
+<div class="headerbanner">
+
+</div>
+
+<div class="menu">
+<a href="https://livedrawsgptoto.shop/" title="LIVE DRAW SGP" target="_blank">LIVE DRAW SGP</a>
+</div>
+<div class="banner1">
+
+</div>
+<div class="content">
+<h1 style="text-align: center;"><a href="/" title="Live Draw SGP Tercepat | Hasil Live Draw Singapore Hari Ini Lengkap"><strong>Live Draw SGP Tercepat | Hasil Live Draw Singapore Hari Ini Lengkap</strong></a></h1>
+<p>Halaman ini menampilkan <strong>Live Draw SGP</strong> secara real-time. Semua hasil akan diperbarui otomatis sehingga Anda bisa langsung mengetahui angka keluaran terbaru tanpa perlu memuat ulang halaman.</p>
+<p>Selain update live, tersedia juga <strong>Data SGP</strong> lengkap sebagai arsip untuk memudahkan pengecekan hasil sebelumnya. Jika Anda baru pertama kali mengakses halaman ini, silakan baca bagian <strong>Panduan</strong> agar lebih mudah memahami cara membaca result dengan benar.</p>
+<p>Semua informasi di sini disusun ringkas, cepat, dan akurat khusus untuk Anda yang membutuhkan hasil <strong>Live Draw SGP</strong> setiap hari.
+</p>
+
+<h2 style="text-align: center;">
+  <a href="/" title="Live Draw SGP">
+    <strong>Live Draw SGP Hari Ini</strong>
+  </a>
+</h2>
+<p>
+  Berikut adalah tayangan <strong>Live Draw SGP Hari Ini</strong> yang diperbarui secara real-time. Angka hasil keluaran akan muncul otomatis sesuai jadwal tanpa perlu memuat ulang halaman. Dengan tampilan ini, Anda bisa langsung memantau hasil terbaru setiap hari dengan cepat dan akurat.
+</p>
+<div style="text-align:center; margin-top:20px;">
+<a href="mailto:tercepatlivedrawhk@gmail.com" class="btn-email">
+  <i class="fa-solid fa-envelope icon-anim"></i> tercepatlivedrawhk@gmail.com
+</a>
+</div>
+<div class="dframe">
+<center>
+  <iframe src="https://rankcrack.com/sgp.php" width="80%" height="700" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin"></iframe>
+</center>
+</div>
+<p>
+  Pastikan koneksi internet Anda stabil agar update hasil dapat tampil dengan lancar. Jika angka belum muncul, silakan tunggu beberapa saat hingga sistem memperbarui hasil secara otomatis.
+<div class="dframe">
+<center>
+  <iframe src="https://rankcrack.com/toto.php" width="80%" height="660" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin"></iframe>
+</center>
+</div>
+
+<h2>
+  <a class="title-head" href="/" title="Data SGP">Data SGP</a>
+</h2>
+<p>
+  Bagian ini menyajikan <strong>Data SGP</strong> secara lengkap dan terupdate. Anda dapat melihat hasil keluaran sebelumnya yang tersusun rapi sebagai arsip harian, mingguan, hingga bulanan. Data ini berguna untuk memudahkan pengecekan angka dan sebagai referensi tambahan.
+</p>
+<div class="dframe">
+<center>
+  <iframe src="https://tabelupdate.online/data-keluaran-singapore/" width="80%" height="800" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin"></iframe>
+</center>
+</div>
+<p>
+  Semua data diperbarui otomatis mengikuti hasil resmi dari <a href="https://en.wikipedia.org/wiki/Live_in_Singapore">Live Draw SGP Singapore</a>, sehingga Anda bisa memastikan angka yang tampil adalah akurat dan terpercaya.
+</p>
+
+<h2 style="text-align: center;">
+  <a href="/" title="Panduan Membaca Result SGP">
+    <strong>Panduan Membaca Result SGP</strong>
+  </a>
+</h2>
+<p>
+  Untuk Anda yang baru pertama kali mengikuti <strong>Live Draw SGP</strong>, bagian ini akan membantu memahami cara membaca hasil keluaran dengan benar. Setiap angka yang muncul memiliki urutan dan arti tertentu sesuai dengan format resmi.
+</p>
+<p>
+  Panduan ini dirancang agar mudah dipahami, sehingga siapa pun dapat memeriksa hasil tanpa kebingungan. Dengan mengikuti langkah-langkah sederhana, Anda bisa memastikan hasil yang ditampilkan sesuai dengan <strong>Data SGP</strong> terbaru.
+</p>
+
+<h2 style="text-align: center;">
+  <a href="/" title="FAQ Live Draw SGP">
+    <strong>FAQ Seputar Live Draw SGP</strong>
+  </a>
+</h2>
+
+<h3>Apa itu Live Draw SGP?</h3>
+<p>
+  Live Draw SGP adalah tayangan hasil keluaran angka Singapore yang ditampilkan secara langsung dan diperbarui real-time setiap hari sesuai jadwal resmi.
+</p>
+
+<h3>Jam berapa Live Draw SGP dimulai?</h3>
+<p>
+  Umumnya Live Draw SGP dimulai pada malam hari. Waktu dapat berbeda sesuai jadwal resmi, tetapi hasil akan otomatis tampil di halaman ini saat update tersedia.
+</p>
+
+<h3>Apakah Data SGP di sini akurat?</h3>
+<p>
+  Ya, semua <strong>Data SGP</strong> yang ditampilkan bersumber dari hasil live draw resmi, sehingga keakuratan data selalu terjaga.
+</p>
+
+<h3>Apakah saya harus memuat ulang halaman untuk melihat update?</h3>
+<p>
+  Tidak, halaman ini bekerja secara otomatis. Hasil akan muncul sendiri tanpa perlu reload, sehingga Anda bisa terus memantau keluaran terbaru.
+</p>
+
+<h3>Apakah tersedia arsip hasil sebelumnya?</h3>
+<p>
+  Tentu, selain live result, Anda juga dapat melihat <strong>Data SGP</strong> berupa arsip keluaran harian, mingguan, hingga bulanan untuk kebutuhan pengecekan.
+</p>
+
+</div>
+</div>					
+</div> 
+<p class="text-center copyright" style="text-align:center; color:#ffffff;">
+  Copyright 2025 © Live Draw SGP
+</p>
+</body>
+</html>
